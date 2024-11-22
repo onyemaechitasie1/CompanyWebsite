814713d28f348cfc06ac39b461250583b2aac59b
GODSTIME TASIE <onyemaechitasie@gmail.com>
2024-11-22 16:34:11 +0100

    Students Project with Contact Us email with st.selectbox

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib, ssl
+
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    user_email = "lisawilson00222@gmail.com"
+    password = "xabjoyncavfgthto"
+    receiver = "onyemaechitasie@gmail.com"
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(user_email, password)
+        server.sendmail(user_email, receiver, message)
